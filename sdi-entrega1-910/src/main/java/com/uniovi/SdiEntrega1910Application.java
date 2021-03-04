0f2b2aae426a198ac2146e978e66ed50cc7c8f71
albertong98 <albertng1998@gmail.com>
2021-03-04 11:59:16 +0100

    First commit

---
+++ sdi-entrega1-910/src/main/java/com/uniovi/SdiEntrega1910Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega1910Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega1910Application.class, args);
+	}
+
+}
