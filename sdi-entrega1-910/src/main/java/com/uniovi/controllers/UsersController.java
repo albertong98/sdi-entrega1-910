fbeb77d48fe2950c7f5879350fbfceae698d82ea
albertong98 <albertng1998@gmail.com>
2021-03-04 13:01:44 +0100

    Inicio de sesión y cerrar sesión

--- sdi-entrega1-910/src/main/java/com/uniovi/controllers/UsersController.java
+++ sdi-entrega1-910/src/main/java/com/uniovi/controllers/UsersController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.uniovi.entities.User;
+import com.uniovi.services.SecurityService;
 import com.uniovi.services.UsersService;
 
 @Controller
 public class UsersController {
 	@Autowired
 	private UsersService usersService;
+	@Autowired
+	private SecurityService securityService;
 	
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signup(Model model) {
@@ -24,6 +27,15 @@ public class UsersController {
 	public String signup(@ModelAttribute User user) {
 		user.setSaldo(100);
 		usersService.addUser(user);
+		securityService.autoLogin(user.getEmail(),user.getPasswordConfirm());
 		return "redirect:home";
+	}
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home() {
+		return "home";
+	}
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
 	}
 }
